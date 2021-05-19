46d99f23c82ba9edade2fed9dda99709c5865cf5
Vanshika <vanshikayadavby@gmail.com>
2021-05-19 22:40:47 +0530

    new changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,7 +15,9 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-export { firebase, database as default };
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
 
 // //child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
